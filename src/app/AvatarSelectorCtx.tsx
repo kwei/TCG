d3e9cdb9f0c51f760ef4c94259210e7e014f08c8
KW <a0979597291@gmail.com>
2024-07-06 20:24:17 +0800

    Game Board UI

--- src/app/AvatarSelectorCtx.tsx
+++ src/app/AvatarSelectorCtx.tsx
@@ -26,31 +26,31 @@ export const AvatarSelectorCtx = ({ children }: { children: ReactNode }) => {
         className={`fixed bottom-0 left-0 right-0 top-0 flex items-center justify-center ${show ? "visible" : "invisible"}`}
       >
         <div
-          className={`h-1/2 w-1/2 rounded-xl bg-dark-base-color p-4 transition-all duration-300 ${show ? "scale-100" : "scale-0"}`}
+          className={`h-1/2 w-[576px] rounded-xl bg-dark-base-color p-8 transition-all duration-300 ${show ? "scale-100" : "scale-0"}`}
         >
-          <div className="flex h-5/6 w-full flex-wrap gap-2 overflow-y-auto overflow-x-hidden">
+          <div className="flex h-[calc(100%-64px)] w-full flex-wrap gap-2 overflow-y-auto overflow-x-hidden p-4">
             {new Array(POKEMON_INDEX_RANGE.max).fill(0).map((_, i) => (
               <button
                 type="button"
                 key={i.toString()}
                 onClick={() => setIndex(i + 1)}
-                className={`flex size-[72px] items-center justify-center rounded-full border-2 border-solid border-frame transition-all ${i + 1 === index ? "scale-110" : "scale-100"}`}
+                className={`flex size-[72px] scale-100 items-center justify-center rounded-full border-2 border-solid border-frame transition-all ${i + 1 === index ? "scale-110 border-light-primary" : "hover:scale-110 hover:border-light-primary"}`}
               >
                 <Image
                   src={`/avatars/${i + 1}.svg`}
-                  className="size-[56px] object-scale-down"
-                  width={56}
-                  height={56}
+                  className="size-[52px] object-scale-down"
+                  width={52}
+                  height={52}
                   alt="Avatar"
                   priority
                 />
               </button>
             ))}
           </div>
-          <div className="flex h-1/6 w-full items-end justify-end">
+          <div className="flex h-[64px] w-full items-end justify-end">
             <button
               type="button"
-              className="rounded-md bg-primary px-3 font-bold text-base-color transition-colors hover:bg-light-primary"
+              className="rounded-md bg-primary px-4 py-1 font-bold text-base-color transition-colors hover:bg-light-primary"
               onClick={() => setShow(false)}
             >
               關閉
